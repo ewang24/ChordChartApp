de787d77158d367c57261f7cbee6b70ca808399a
E. R. Wang <01ewang24@gmail.com>
2023-01-20 22:56:48 -0700

    trying to get spring working

--- ChordChartApp/src/main/java/com/oneandahalfasians/chordchartapp/data/ChartService.java
+++ ChordChartApp/src/main/java/com/oneandahalfasians/chordchartapp/data/ChartService.java
@@ -2,7 +2,6 @@ package com.oneandahalfasians.chordchartapp.data;
 
 import com.oneandahalfasians.chordchartapp.data.entities.ChartEntity;
 
-
 public class ChartService implements IChartService{
 
 
