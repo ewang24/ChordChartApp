a3298ee780b9cf1ab42fc4644b282b34f28c5a27
winstonji <winston.wy.ji@gmail.com>
2023-02-03 19:44:38 -0700

    I think we finally successfully committed sudoku on the .xml files

--- ChordChartApp/src/main/java/com/oneandahalfasians/chordchartapp/controller/MainController.java
+++ ChordChartApp/src/main/java/com/oneandahalfasians/chordchartapp/controller/MainController.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
-    //random test comment
     @FXML
     public GridPane appCenterVBox;
 
